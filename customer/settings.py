6a28a7247a50b697649bede499c81d0639988a8d
BaBa7080 <abhishekshukla0700@gmail.com>
2024-09-22 00:57:55 +0530

    remove

--- customer/settings.py
+++ customer/settings.py
@@ -61,8 +61,8 @@ MIDDLEWARE = [
 CORS_ALLOW_ALL_ORIGINS = True
 CORS_ALLOW_ALL_ORIGINS = True
 CORS_ALLOWED_ORIGINS = [
-    'http://localhost:3000/',
-    'http://localhost:5173/'
+    'http://localhost:3000',
+    'http://localhost:5173'
 ]
 ROOT_URLCONF = 'customer.urls'
 
